4a655d5a9d8c00df9d6d9f14aa7cc351db481ad9
Michał Bagrowski <m.bagrowski@fivedottwelve.com>
2023-07-06 14:30:09 +0200

    Initial Commit

---
+++ mb_weatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mb_weatherApp
+//
+//  Created by Michał Bagrowski on 06/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
